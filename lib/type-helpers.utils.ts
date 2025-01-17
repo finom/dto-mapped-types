c9181ce12353aaab269b925a331f0969c58e6249
Andrii Gubanov <andrii@gubanov.eu>
2025-01-17 16:23:46 +0100

    fix: Use latest class-transformer

--- lib/type-helpers.utils.ts
+++ lib/type-helpers.utils.ts
@@ -133,14 +133,8 @@ function inheritTransformerMetadata(
   isPropertyInherited?: (key: string) => boolean,
   stackDecorators = true,
 ) {
-  let classTransformer: any;
-  try {
-    /** "class-transformer" >= v0.3.x */
-    classTransformer = require('class-transformer/cjs/storage');
-  } catch {
-    /** "class-transformer" <= v0.3.x */
-    classTransformer = require('class-transformer/storage');
-  }
+  const classTransformer = require('class-transformer/cjs/storage');
+
   const metadataStorage /*: typeof import('class-transformer/types/storage').defaultMetadataStorage */ =
     classTransformer.defaultMetadataStorage;
 
